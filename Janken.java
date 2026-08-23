a2f6bf51280ddb9f391ff32956495ba312dbbd42
takathius2 <talahashi760@gmail.com>
2026-08-23 23:24:44 +0900

    勝敗判定を実装

--- Janken.java
+++ Janken.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 import java.util.Random;
 
+
 public class Janken{
+	static String getHandName(int hand){
+		if (hand ==1){
+			return "グー✊";
+		}else if (hand == 2){
+			return "チョキ✌";
+		}else{
+			return "パー✋";
+		}
+	}
+
+
 	public static void main(String[]args){
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
@@ -16,14 +28,30 @@ public class Janken{
 		System.out.println();
 		
 		System.out.println("番号を入力してください");
-
+		
 		int playerHand = scanner.nextInt();
 		int cpuHand = random.nextInt(3) + 1;
 
+		String playerHandName = getHandName(playerHand);
+		String cpuHandName = getHandName(cpuHand);
+
+		System.out.println("あなた：" + playerHandName);
+                System.out.println("CPU:" + cpuHandName);
+
+		//判定
+	        if (playerHand == cpuHand){
+			System.out.println("あいこです！");
+		}else if ((playerHand ==1 && cpuHand ==2)||(playerHand == 2 && cpuHand == 3)||(playerHand == 3 && cpuHand == 1))
+				{
+					System.out.println("あなたの勝ち！");
+				}else{
+					System.out.println("あなたの負け！");
+				}
+	
 		scanner.close();
 
-		//テスト用
-		//System.out.println("入力された番号" + playerHand);
-		System.out.println("CPU:" + cpuHand);
+
+
+
 	}
 }
