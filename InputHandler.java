64db1b5dc3273383aeab6afdf2f12c6fad0d6148
takathius2 <talahashi760@gmail.com>
2026-09-15 07:00:04 +0900

    入力チェック機能の実装(途中)

---
+++ InputHandler.java
@@ -0,0 +1,14 @@
+import java.utill.scanner;
+public class InputHandler{
+	private Scanner scanner = new Scanner(System.in);
+
+	public int getPlayerHand(){
+		while(true){
+			System.out.println("番号を入力してください");
+			
+			System.out.println("番号を入力してください");
+			System.out.println("番号を入力してください");
+			System.out.println("番号を入力してください");
+			System.out.println("番号を入力してください");
+}
+
