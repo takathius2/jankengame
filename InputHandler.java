adcf95af7ab85196fcbc0e6523f804534839ee35
takathius2 <talahashi760@gmail.com>
2026-09-17 07:02:49 +0900

    入力チェック機能の実装(メソッド作成)

--- InputHandler.java
+++ InputHandler.java
@@ -1,4 +1,4 @@
-import java.utill.scanner;
+import java.util.Scanner;
 public class InputHandler{
 	private Scanner scanner = new Scanner(System.in);
 
@@ -6,9 +6,17 @@ public class InputHandler{
 		while(true){
 			System.out.println("番号を入力してください");
 			
-			System.out.println("番号を入力してください");
-			System.out.println("番号を入力してください");
-			System.out.println("番号を入力してください");
-			System.out.println("番号を入力してください");
-}
+			System.out.println("1:グー");
+			System.out.println("2:チョキ");
+			System.out.println("3:パー");
 
+			int hand = scanner.nextInt();
+
+			if(hand >= 1&& hand <= 3){
+				return hand;
+			}
+
+			System.out.println("1~3の数字を入力してください");
+		}
+	}
+}
